f0e287e5f4765a93bba172738180def08d33296c
sudeep888 <your-email@example.com>
2026-02-12 00:24:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import seatRoutes from "./routes/seatRoutes.js";
+import showtimeRoutes from "./routes/showtimeRoutes.js";
+import theatreRoutes from "./routes/theatreRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MovieMatrix API Running 🎬");
+});
+
+app.use(
+  "/api/theatres",
+   theatreRoutes
+  );
+app.use(
+  "/api/showtimes", 
+  showtimeRoutes
+  );
+app.use(
+  "/api/seats",
+  seatRoutes
+);
+
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
