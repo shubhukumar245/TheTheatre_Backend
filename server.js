02b95c840ebc22094866ce149ff08070d145d068
kumar shubham <shubhamkumar24517@gmail.com>
2026-02-12 01:37:37 +0530

    Restrict CORS to localhost:5173

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
