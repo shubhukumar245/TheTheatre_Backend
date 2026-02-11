cdfce09dd3dc6d012703c3e76cb2905615eaf791
kumar shubham <shubhamkumar24517@gmail.com>
2026-02-12 03:55:35 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://the-theatre-frontend.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
